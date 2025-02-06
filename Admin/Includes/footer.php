613c6f1116dbca321ee72a7b054c63ea1a245205
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-02-06 16:14:20 +0545

    Project Completion

--- Admin/Includes/footer.php
+++ Admin/Includes/footer.php
@@ -1,8 +1,7 @@
  <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by NATIONAL DIPLOMA
-              STUDENT ATTENDANCE MANAGEMENT SYSTEM
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by XXXXX
             </span>
           </div>
         </div>
